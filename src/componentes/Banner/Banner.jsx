853683341c361101cf0a759faf96cb1d79163626
Geovana Frota <geovanafrota2019@gmail.com>
2024-12-17 11:59:37 -0300

    first commit

--- src/componentes/Banner/Banner.jsx
+++ src/componentes/Banner/Banner.jsx
@@ -1,11 +1,9 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className="banner">
             <img src="/imagens/banner.png" alt="Banner principal da página Organo"></img>
         </header>
     );
 }
-
-export default Banner
